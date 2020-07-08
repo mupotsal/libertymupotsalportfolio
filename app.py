c2f91e6bc04b8af1b0310ebd66b8d3228f47b67d
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-07-08 01:18:22 -0400

    waiting for deploy

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/templates/about.html')
+def about():
+	return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+	return render_template('portfolio.html')
+@app.route('/github')
+def github():
+	return render_template('github.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
 if __name__ == '__main__':
 	app.run()
